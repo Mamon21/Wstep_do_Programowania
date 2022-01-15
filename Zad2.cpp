8dd524eb862abacda7daa7b47430bdd87ae32452
Mamon21 <93038092+Mamon21@users.noreply.github.com>
2022-01-15 09:56:54 +0100

    Add files via upload

---
+++ Zad2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    float tab[10];
+
+
+    for(int i=0;i<10;i++){
+        cout<<"Podaj "<<i+1 <<" liczbe:"<<endl;
+
+    cin>>tab[i];
+
+    cout<<endl;
+    }
+        cout<<"Tablica odwrocona:    "
+
+    for(int i=9;i>=0;i--){
+
+
+        cout<<tab[i]<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
